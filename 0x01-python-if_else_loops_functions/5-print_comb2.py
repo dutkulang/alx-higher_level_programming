be390a698d5177ee655bdc1314940dacbd650981
Dukula <dutandrew78@gmail.com>
2023-01-04 03:44:07 +0300

    printing numbers from 00-99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(100):
-    if i <= 9:
-        print("0{}".format(i), end=", ")
-    else:
-        print("{}".format(i), end=", ")
+for i in range(99):
+    print("{:02d}".format(i), end=", ")
+print("{:02d}".format(i + 1))
