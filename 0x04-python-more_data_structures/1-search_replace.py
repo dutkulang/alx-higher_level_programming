ca4b4be00933850748993539941461b78e97fecd
Dut Kulang <dutandrew78@gmail.com>
2023-01-15 20:26:52 +0300

    i was supposed to use lambda and map functions

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-
-    return my_list
+    x = map((lambda num: replace if search == num else num), my_list)
+    return list(x)
