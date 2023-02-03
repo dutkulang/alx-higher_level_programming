935bfadb085c0ed60d96f5522ed40de5ab1dc7ed
Dut Kulang <dutandrew78@gmail.com>
2023-02-03 16:33:26 +0300

    size as a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Defining a square class """
+
+
+class Square:
+    """
+    Square with size as private attribute
+    """
+    def __init__(self, size):
+        self.__size = size
+        
