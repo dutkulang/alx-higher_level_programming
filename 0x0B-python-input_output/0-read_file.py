88d910bb198a38418912cc41d6e00c45a357970c
Dut Kulang <dutandrew78@gmail.com>
2023-02-13 19:07:53 +0300

    reading content of file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+""" Defines a text reading function"""
+
 
 def read_file(filename=""):
+    """reads the content of a files and prints it on to the screen"""
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
-    
-
-read_file('text.txt')
