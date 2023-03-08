724e21c29dea964575f8ecdec1c91b456d0608ad
Dukula <dutandrew78@gmail.com>
2023-03-08 14:04:25 +0300

    add two number or return TypeError if one or both is a none integer or float

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,4 +5,17 @@ Adding integers
 """
 
 def add_integer(a, b=98):
-    pass
+    """
+    Return sum of a and b
+    floats must be typecasted to integers
+    Raises TypeError: If a or b is not an integer or float   
+    """
+
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    
+    elif (not isinstance(b, int) and not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
