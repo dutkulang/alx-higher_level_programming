118c2a63606255d6cbca055a8f0468e7d9c197af
Dukula <dutandrew78@gmail.com>
2023-03-08 16:36:50 +0300

    print out first and last name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+say_my_name prints out the first and last name passed to it.
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+
+    """
+    Return the first and last name
+    Raises TypeError if first name or last name are not strings.
+    """
+
+    if (not isinstance(first_name, str)):
+        raise TypeError("first_name must be a string")
+
+    elif (not isinstance(last_name, str)):
+        raise TypeError("last_name must be a string")
+    return f"My name is {first_name} {last_name}"
