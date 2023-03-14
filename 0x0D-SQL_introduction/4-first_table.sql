2bc6e7970cf2f8e5bcbc62859d7271beda60f384
Dukula <dutandrew78@gmail.com>
2023-03-14 14:01:26 +0300

    create table and return no error if the table already exists

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,2 @@
 -- Create a database table and return no erro if the table already exists
-CREATE TABLE IF NOT EXISTS first_table
-       (id INT,
-       name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
