3e5eae27eaf5f33ef67c2f12f2edcb79129357b9
Dukula <dutandrew78@gmail.com>
2023-03-15 05:23:44 +0300

    gone

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- maximum temperature of each state
-SELECT `state`, max(value) as `maximum_temperature` FROM `temperatures` GROUP BY `state` ORDER BY `state`;
+SELECT `state`, max(`value`) as `max_temp` FROM `temperatures` GROUP BY `state` ORDER BY `state` LIMIT 3;
