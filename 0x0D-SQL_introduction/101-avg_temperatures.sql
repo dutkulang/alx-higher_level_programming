4493281518b192e261b5185f4abebe57ac758796
Dukula <dutandrew78@gmail.com>
2023-03-15 05:16:30 +0300

    gone

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 --Sorted average temperature of cities
-SELECT `city`, AVG(`value`) AS `avg_temp`FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
