b9fb27ee62351d204547fa0373c835c5abedfe14
Dukula <dutandrew78@gmail.com>
2023-03-15 05:10:47 +0300

    gone

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- list the 3 most hotests cities
-SELECT `city`, AVG(`value`) AS `avg` FROM `temperatures` WHERE `month` = 7 OR `month` = 8 GROUP BY `city` ORDER BY `avg` DESC LIMIT 3;
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` WHERE `month` = 7 OR `month` = 8 GROUP BY `city` ORDER BY `avg` DESC LIMIT 3;
