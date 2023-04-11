836191aba10141eeac917e15ee451ead91a647a5
Dut <dutandrew78@gmail.com>
2023-04-11 12:46:24 +0300

    getting command line arguments js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,8 @@
 
 const myVar = process.argv.slice(2);
 
-if (myVar < 2){
-	console.log('No argument');
-}
-else{
-	console.log('Argument found');
+if (myVar < 2) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
 }
