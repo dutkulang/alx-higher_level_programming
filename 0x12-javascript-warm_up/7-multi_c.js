be917a5d1a0e87a2e51b36b2200673b07bbaa6e9
Dut <dutandrew78@gmail.com>
2023-04-12 06:32:16 +0300

    printing based on the arg passed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-const  myvar = process.argv.slice(2);
+const myvar = process.argv.slice(2);
 const mystr = 'C is fun';
 let i = 0;
 
-if (myvar.length === 0){
-    console.log('Missing number of occurrences');
+if (myvar.length === 0) {
+  console.log('Missing number of occurrences');
 }
-while (i < parseInt(process.argv[2])){
-    console.log(mystr);
-    i++;
+while (i < parseInt(process.argv[2])) {
+  console.log(mystr);
+  i++;
 }
