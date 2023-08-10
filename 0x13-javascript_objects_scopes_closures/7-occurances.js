e597ca7a67e503c9c4947b19688adac3bed7e2fe
Dut <dutandrew78@gmail.com>
2023-08-10 19:07:34 +0300

    fixed typo

--- 0x13-javascript_objects_scopes_closures/7-occurances.js
+++ 0x13-javascript_objects_scopes_closures/7-occurances.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-exports.nbOccurances = function (List, searchElement){
+exports.nbOccurences= function (List, searchElement){
   return List.reduce((m, x) => x === searchElement ? m +1 : m, 0);
 };
