7633347b2aee7736f572911e5d4df53347955555
Dut <dutandrew78@gmail.com>
2023-08-10 19:15:17 +0300

    occurence function

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-exports.nbOccurences = function (List, searchElement){
-  return List.reduce((m, x) => x === searchElement ? m +1 : m, 0);
+exports.nbOccurences = function (List, searchElement) {
+  return List.reduce((m, x) => x === searchElement ? m + 1 : m, 0);
 };
