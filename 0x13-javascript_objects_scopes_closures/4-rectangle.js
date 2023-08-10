a9995616a176bb35eddf6247b5b2396b998079e1
Dut <dutandrew78@gmail.com>
2023-08-10 18:37:06 +0300

    added 4th rectangle class with route function

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -19,7 +19,6 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    this.width = this.height;
-    this.height = this.width;
+    [this.width, this.height] = [this.height, this.width];
   }
 };
